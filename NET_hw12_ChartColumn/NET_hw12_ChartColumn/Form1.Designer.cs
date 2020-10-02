0fce7d87c0b92f50948b30ab60bf73158eeaa3ea
Perry <49940223+parry1233@users.noreply.github.com>
2020-10-02 21:56:21 +0800

    Add files via upload

---
+++ NET_hw12_ChartColumn/NET_hw12_ChartColumn/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace NET_hw12_ChartColumn
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 設計工具所需的變數。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清除任何使用中的資源。
+		/// </summary>
+		/// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form 設計工具產生的程式碼
+
+		/// <summary>
+		/// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+		/// 這個方法的內容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label4 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.label4.Location = new System.Drawing.Point(239, 68);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(120, 25);
+			this.label4.TabIndex = 12;
+			this.label4.Text = "Female(%) :";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.label3.Location = new System.Drawing.Point(261, 16);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(98, 25);
+			this.label3.TabIndex = 11;
+			this.label3.Text = "Male(%) :";
+			// 
+			// textBox2
+			// 
+			this.textBox2.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.textBox2.Location = new System.Drawing.Point(116, 65);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(100, 34);
+			this.textBox2.TabIndex = 10;
+			this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.label2.ForeColor = System.Drawing.Color.Red;
+			this.label2.Location = new System.Drawing.Point(20, 68);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(90, 25);
+			this.label2.TabIndex = 9;
+			this.label2.Text = "Female: ";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.textBox1.Location = new System.Drawing.Point(116, 13);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 34);
+			this.textBox1.TabIndex = 8;
+			this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.label1.ForeColor = System.Drawing.Color.Blue;
+			this.label1.Location = new System.Drawing.Point(42, 16);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(68, 25);
+			this.label1.TabIndex = 7;
+			this.label1.Text = "Male: ";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(579, 567);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label1;
+	}
+}
+
---
+++ NET_hw12_ChartColumn/NET_hw12_ChartColumn/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NET_hw12_ChartColumn
+{
+	public partial class Form1 : Form
+	{
+		private int Max_Height;
+		private int Max_Width;
+		private int Uni_Width;
+		private double[] person;
+		private Graphics g;
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			this.g = this.CreateGraphics();
+			this.person = new double[2];
+			this.Max_Height = 300;
+			this.Max_Width = 350;
+			this.Uni_Width = 80;
+		}
+
+		private void Form1_Paint(object sender, PaintEventArgs e)
+		{
+			Pen p = new Pen(Color.Black, 5);
+			g.DrawLine(p, 25, 134, 25, (134+this.Max_Height));
+			g.DrawLine(p, 25, (134 + this.Max_Height), (25+this.Max_Width), (134 + this.Max_Height));
+		}
+
+		private void textBox1_TextChanged(object sender, EventArgs e)
+		{
+			count(textBox1.Text, textBox2.Text);
+		}
+
+		private void textBox2_TextChanged(object sender, EventArgs e)
+		{
+			count(textBox1.Text, textBox2.Text);
+		}
+
+		private void count(string male, string female)
+		{ 
+			try
+			{
+				person[0] = Convert.ToDouble(male);
+			}
+			catch(System.FormatException)
+			{
+				person[0] = 0;
+			}
+			catch(System.NullReferenceException)
+			{
+				person[0] = 0;
+			}
+
+			try
+			{
+				person[1] = Convert.ToDouble(female);
+			}
+			catch (System.FormatException)
+			{
+				person[1] = 0;
+			}
+			catch (System.NullReferenceException)
+			{
+				person[1] = 0;
+			}
+
+			double sum = this.person[0] + this.person[1];
+			double percent_Male = this.person[0] / sum;
+			double percent_Female = this.person[1] / sum;
+			label3.Text = "Male(%) :" + (percent_Male*100).ToString("0.0") + " %";
+			label4.Text = "Female(%) :" + (percent_Female*100).ToString("0.0") + " %";
+			paint(percent_Male, percent_Female);
+		}
+
+		private void paint(double p_male,double p_female)
+		{
+			this.Refresh();
+
+			//Pen pen_male = new Pen(Color.Blue, 5);
+			SolidBrush brush_male = new SolidBrush(Color.Blue);
+			Rectangle r_male = new Rectangle((350 / 3 - 100 / 2), Convert.ToInt32((134+this.Max_Height)-300 * p_male), this.Uni_Width, Convert.ToInt32(this.Max_Height * p_male));
+			g.FillRectangle(brush_male, r_male);
+
+			//Pen pen_female = new Pen(Color.Red, 5);
+			SolidBrush brush_female = new SolidBrush(Color.Red);
+			Rectangle r_female = new Rectangle((350 / 3*2 - 100 / 2), Convert.ToInt32((134 + this.Max_Height) - 300 * p_female), this.Uni_Width, Convert.ToInt32(this.Max_Height * p_female));
+			g.FillRectangle(brush_female, r_female);
+
+			Pen p = new Pen(Color.Black, 5);
+			g.DrawLine(p, 25, 134, 25, (134 + this.Max_Height));
+			g.DrawLine(p, 25, (134 + this.Max_Height), (25 + this.Max_Width), (134 + this.Max_Height));
+		}
+	}
+}
